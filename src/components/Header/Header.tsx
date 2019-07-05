82238355769deada2fc60ee5872403201fcac205
nipun-jasti <nipunjasti@gmail.com>
2019-07-05 14:48:24 +1200

    Move project files

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className='headerText'>🤔 How Old? 🤔</h1>
+        </div>
+    )
+}
